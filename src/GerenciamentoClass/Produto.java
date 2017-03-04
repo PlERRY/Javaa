4f59a3c633db9c0ddf418323dcd0fad949c2cdef
finotty <hugo.finotty10@gmail.com>
2017-03-04 18:02:10 -0300

    Quase pronto

---
+++ src/GerenciamentoClass/Produto.java
@@ -0,0 +1,43 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package GerenciamentoClass;
+
+/**
+ *
+ * @author hugo
+ */
+public class Produto {
+   long idProdutoUnit;
+   String nome;
+   float valor_compra; 
+   float valor_venda;
+   int quantidade;
+   int quantidade_min;
+   float lucro_produto;
+   
+   public void registrar(){
+       
+     }
+   
+   public int buscarProd(int x){
+       
+   return x;
+   }
+   
+   public void atualizarProd(){
+   
+   
+   } 
+   
+   public int notificar_prod_acabando(int x){
+   
+   return x;
+   }
+   
+   
+   
+   
+}
